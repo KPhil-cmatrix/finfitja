e9aee61a24854d69178e9458051e70460af9189d
Hali <158500614+KPhil-cmatrix@users.noreply.github.com>
2026-04-01 02:10:28 -0500

    Add matcher.py for Recommendation Generator page
    This file manages the Recommendation Generator page for the FinFit JA Streamlit app. (Page Skeleton)

---
+++ pages/matcher.py
@@ -0,0 +1,12 @@
+"""
+Developer's Name: Khalia Phillips
+App Name: FinFit JA
+Version: 1.0
+Purpose (File): This file manages the Recommendation Generator page for the FinFit JA Streamlit app.
+"""
+
+from layout import hold_page
+
+#Recommendation Generator
+def open_matcher():
+    hold_page("Recommendation Generator", "This page will contain structured banking inputs with recommendation results beside them.")
