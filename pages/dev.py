fb2bb511e4fdfb35a2c854b3600c667764e53c4b
Hali <158500614+KPhil-cmatrix@users.noreply.github.com>
2026-04-01 02:12:05 -0500

    Add dev.py to manage the Dev Process page
    This file manages the Dev Process page for the FinFit JA Streamlit app. (Page Skeleton)

---
+++ pages/dev.py
@@ -0,0 +1,12 @@
+"""
+Developer's Name: Khalia Phillips
+App Name: FinFit JA
+Version: 1.0
+Purpose (File): This file manages the Dev Process page for the FinFit JA Streamlit app.
+"""
+
+from layout import hold_page
+
+#Dev Process
+def open_build():
+    hold_page("Dev Process", "This page will outline the development process, major challenges, and the solutions used.")
