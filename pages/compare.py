265fb10059bd2b7e264d7cfe590de68bf6b33384
Hali <158500614+KPhil-cmatrix@users.noreply.github.com>
2026-04-01 02:11:22 -0500

    Add compare.py for Comparison Profile page
    (Page Skeleton)

---
+++ pages/compare.py
@@ -0,0 +1,12 @@
+"""
+Developer's Name: Khalia Phillips
+App Name: FinFit JA
+Version: 1.0
+Purpose (File): This file manages the Comparison Profile page for the FinFit JA Streamlit app.
+"""
+
+from layout import hold_page
+
+#Comparison Profile
+def open_compare():
+    hold_page("Comparison Profile", "This page will contain tools for comparing banks and account types.")
