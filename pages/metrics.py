82a020a08ccdc82af738541ee115f70b39c51e83
Hali <158500614+KPhil-cmatrix@users.noreply.github.com>
2026-04-01 02:12:36 -0500

    Create metrics.py for Performance Metrics page
    This file manages the Performance Metrics page for the FinFit JA Streamlit app. (Page Skeleton)

---
+++ pages/metrics.py
@@ -0,0 +1,12 @@
+"""
+Developer's Name: Khalia Phillips
+App Name: FinFit JA
+Version: 1.0
+Purpose (File): This file manages the Performance Metrics page for the FinFit JA Streamlit app.
+"""
+
+from layout import hold_page
+
+#Performance Metrics
+def open_scores():
+    hold_page("Performance Metrics", "This page will show testing results, performance metrics, and evaluation notes.")
